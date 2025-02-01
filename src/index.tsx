99c7904c63c3272de481b54ac9e975d2f3a0c92f
Krzysztof Kozak <krzysztofkoz6@gmail.com>
2025-02-01 16:14:57 +0100

    add react query provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,15 @@ import { App } from "./App";
 
 const root = document.getElementById("root") as HTMLElement;
 
-ReactDOM.createRoot(root).render(<App />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(root).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
