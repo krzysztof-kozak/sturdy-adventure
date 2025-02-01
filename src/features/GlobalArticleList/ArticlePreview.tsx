18eceb87ed63a1489324f068aeb019a74ae21cd3
Krzysztof Kozak <krzysztofkoz6@gmail.com>
2025-02-01 21:19:38 +0100

    fix incorrect profile links

--- src/features/GlobalArticleList/ArticlePreview.tsx
+++ src/features/GlobalArticleList/ArticlePreview.tsx
@@ -11,14 +11,14 @@ function ArticlePreview({ title, description, author, createdAt, favoritesCount,
   return (
     <div className="article-preview">
       <div className="article-meta">
-        <Link to={slug}>
+        <Link to={`/profile/${author.username}`}>
           <UserImage url={author.image}>
             <div className="author-image-fallback">{author.username.charAt(0)}</div>
           </UserImage>
         </Link>
 
         <div className="info">
-          <Link to={slug} className="author">
+          <Link to={`/profile/${author.username}`} className="author">
             {author.username}
           </Link>
           <span className="date">{formatDate(createdAt)}</span>
